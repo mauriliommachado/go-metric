4b14ac00911b6a3a5bc641ff5a9f400c5a88813b
Maurílio <mauriliommachado@gmail.com>
2021-01-07 00:06:05 +0000

    add max undo

--- engine/metric-operations.go
+++ engine/metric-operations.go
@@ -16,20 +16,32 @@ func (wapi *WorkerAPI) doMax(metric storage.MetricDefinition, event storage.Even
 	}
 	key := metric.GetDefinitionKey(event)
 	metricValue, ok := wapi.Storage.Get(key)
-	if !ok {
-		metricValue = storage.MetricItem{Key: key, Value: (1 << bits.UintSize) / -2}
-	}
 	eventValue, _ := strconv.Atoi(fmt.Sprintf("%v", event.Data[metric.Aggregation]))
-	if eventValue > metricValue.Value {
-		metricValue.Value = eventValue
-		//saving calculated metric value
+	if inputEvent {
+		if !ok {
+			metricValue = storage.MetricItem{Key: key, Value: (1 << bits.UintSize) / -2}
+		}
+		if eventValue > metricValue.Value {
+			metricValue.Value = eventValue
+			wapi.Storage.Put(key, metricValue)
+		}
+	} else if eventValue == metricValue.Value { // check if the event leaving the window is the current max
+		partialValue := (1 << bits.UintSize) / -2
+		for _, item := range wapi.deltaSlice.Items {
+			itemValue, _ := strconv.Atoi(fmt.Sprintf("%v", item.Data[metric.Aggregation]))
+			if itemValue > partialValue && itemValue != metricValue.Value {
+				partialValue = itemValue
+			}
+		}
+		metricValue.Value = partialValue
 		wapi.Storage.Put(key, metricValue)
 	}
+
 }
 
 func (wapi *WorkerAPI) doMin(metric storage.MetricDefinition, event storage.Event, inputEvent bool, wg *sync.WaitGroup) {
-	key := metric.GetDefinitionKey(event)
 	defer wg.Done()
+	key := metric.GetDefinitionKey(event)
 	metricValue, ok := wapi.Storage.Get(key)
 	eventValue, _ := strconv.Atoi(fmt.Sprintf("%v", event.Data[metric.Aggregation]))
 	if inputEvent {
@@ -40,7 +52,7 @@ func (wapi *WorkerAPI) doMin(metric storage.MetricDefinition, event storage.Even
 			metricValue.Value = eventValue
 			wapi.Storage.Put(key, metricValue)
 		}
-	} else if eventValue == metricValue.Value {
+	} else if eventValue == metricValue.Value { // check if the event leaving the window is the current min
 		partialValue := (1<<bits.UintSize)/2 - 1
 		for _, item := range wapi.deltaSlice.Items {
 			itemValue, _ := strconv.Atoi(fmt.Sprintf("%v", item.Data[metric.Aggregation]))
